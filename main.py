156e80182db9bdf4d1368151bee355a797699681
Тимофей <porno_mail@bk.ru>
2021-05-19 01:01:18 +0400

    repair

--- main.py
+++ main.py
@@ -12,25 +12,33 @@
 # Делаем zip с листам из пункта 2
 # К пустому листу из пункта 7 += zip объект из пункта 10
 # Далее cook_book[Название блюда] = список из 11 пункта
-data = {}
-key = ['ingredient_name', 'quantity', 'measure']
-with open('c_book.txt', 'r', encoding='utf-8') as f:
-    while True:
-        ingredients = []
-        name = f.readline().rstrip()
-        if not name:
-            break
-        ingredient_count = f.readline().rstrip()
-        for i in range(int(ingredient_count)):
-            ing = f.readline().rstrip()
-            ing_list = ing.strip().split("|")
-            ingredient = dict(zip(key, ing_list))
-            ingredient['quantity'] = int(ingredient['quantity'])
-            ingredients.append(ingredient)
-        data[name] = ingredients
-        f.readline().rstrip()
-    print(data)
+def read_cook_book(file):
+    data = {}
+    key = ['ingredient_name', 'quantity', 'measure']
+    with open(file, 'r', encoding='utf-8') as f:
+        while True:
+            ingredients = []
+            name = f.readline().rstrip()
+            if not name:
+                break
+            ingredient_count = f.readline().rstrip()
+            for i in range(int(ingredient_count)):
+                ing = f.readline().rstrip()
+                ing_list = ing.strip().split("|")
+                ingredient = dict(zip(key, ing_list))
+                ingredient['quantity'] = int(ingredient['quantity'])
+                ingredients.append(ingredient)
+            data[name] = ingredients
+            f.readline().rstrip()
+    return data
 
+
+file = 'c_book.txt'
+data = read_cook_book(file)
+print(data)
+
+
+# print(read_cook_book('c_book.txt'))
 
 def get_shop_list_by_dishes(dishes, person_count):
     """
